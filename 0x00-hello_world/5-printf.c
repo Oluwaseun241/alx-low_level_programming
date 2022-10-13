ab1d2f253eaba9f63105972febba55adb9b5bf22
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2022-10-13 01:46:31 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,"\n");
+	return (0);
+}
