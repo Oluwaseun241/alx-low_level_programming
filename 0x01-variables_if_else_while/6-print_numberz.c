f8de151ba70e37ff984859b313787960e0760c80
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2022-10-14 09:51:16 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all numbers under 10
+ * without using any char variables
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+
+	}
+	putchar('\n');
+	return (0);
+}
