1863cb51125263c1194a927822c54c6b96d623c1
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2022-10-14 10:28:52 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(0);
+		a--;
+	}
+	
+	putchar('\n');
+	return (0);
+}
