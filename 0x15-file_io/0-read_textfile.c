1c3d5d2dc492e24b46d614e09f24920ffac60f41
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2022-12-05 13:01:34 +0100

    'auto'

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,55 +1,53 @@
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include "main.h"
 
 /**
- * read_textfile - read and print from a file
+ * read_textfile - reads and prints from a file
  * @filename: path to file
  * @letters: chars to read
  * Return: chars read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  char *buff;
-  ssize_t bytes, r;
+	int fd;
+	char *buff;
+	ssize_t bytes, r;
 
-  if (!filename)
-    return (0);
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-  {
-    close(fd);
-    return (0);
-  }
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
 
-  buff = malloc(sizeof(char) * letters);
-  if (!buff)
-  {
-    close(fd);
-    return (0);
-  }
-  
-  bytes = read(fd, buff, letters);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+	{
+		close(fd);
+		return (0);
+	}
 
-  if (bytes == -1)
-  {
-    close(fd);
-    free(buff);
-    return (0);
-  }
-   r = write(STDOUT_FILENO, buff, bytes);
+	bytes = read(fd, buff, letters);
 
-   if (r== -1)
-   {
-     close(fd);
-     free(buff);
-     return (0);
-   }
+	if (bytes == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
 
-   close(fd);
-   return (bytes);
+	r = write(STDOUT_FILENO, buff, bytes);
 
+	if (r == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+	close(fd);
+	return (bytes);
 }
-
